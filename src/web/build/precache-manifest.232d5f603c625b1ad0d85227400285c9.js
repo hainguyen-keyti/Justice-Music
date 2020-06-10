103acbc3197293989fe4c86d9d7e77ee36f502c1
keyti <nhonnh9939@gmail.com>
2020-06-10 22:46:55 +0700

    change server address

---
+++ src/web/build/precache-manifest.232d5f603c625b1ad0d85227400285c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82e0a8fcb22064b0ee4c81c71ab1933",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16cce10bbdee8140781f",
+    "url": "/static/css/2.d73e1a58.chunk.css"
+  },
+  {
+    "revision": "f2f1c31fd74ddb8ec624",
+    "url": "/static/css/main.b4b45136.chunk.css"
+  },
+  {
+    "revision": "16cce10bbdee8140781f",
+    "url": "/static/js/2.7400009e.chunk.js"
+  },
+  {
+    "revision": "10d5bda70f7ec65127a77fe0786b36da",
+    "url": "/static/js/2.7400009e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f1c31fd74ddb8ec624",
+    "url": "/static/js/main.c4266fba.chunk.js"
+  },
+  {
+    "revision": "b79d61d614db827975ea",
+    "url": "/static/js/runtime-main.f23d09bb.js"
+  },
+  {
+    "revision": "6014fc10115c163ef03f11db4ef4654a",
+    "url": "/static/media/logo.6014fc10.png"
+  }
+]);
--- src/web/build/service-worker.js
+++ src/web/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94936722bcb8d51a5184880d711266e2.js"
+  "/precache-manifest.232d5f603c625b1ad0d85227400285c9.js"
 );
 
 self.addEventListener('message', (event) => {
